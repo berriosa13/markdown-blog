ffde3bcbb65e9e727c619b403f8ceafbe1da67fc
berriosa13 <54926487+berriosa13@users.noreply.github.com>
2021-06-14 23:59:33 -0400

    first working version

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article')
-const app = express();
 const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override')
+const app = express();
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
@@ -11,12 +12,11 @@ mongoose.connect('mongodb://localhost/blog', {
 });
 
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({extended: false }))
+app.use(methodOverride('_method'))
 
 app.get("/", async (req, res) => {
-    const articles = await Article.find().sort({
-        createdAt: 'desc' })
+    const articles = await Article.find().sort({createdAt: 'desc' })
     res.render('articles/index', { articles: articles });
 });
 
